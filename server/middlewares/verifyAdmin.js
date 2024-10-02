e7aa0e2cc4aeb5feda514b1c886ddf7f1ae6b2d6
xedter20 <dextermiranda441@gmail.com>
2024-10-02 08:22:03 +0800

    update

--- server/middlewares/verifyAdmin.js
+++ server/middlewares/verifyAdmin.js
@@ -1,8 +1,8 @@
 const verifyAdmin = async (req, res, next) => {
   const email = req.decoded.email;
-  const adminEmail = "admin@urbanaura.com";
+  const adminEmail = 'admin_handihub@gmail.com';
   if (email !== adminEmail) {
-    return res.status(403).send({ error: true, message: "forbidden access" });
+    return res.status(403).send({ error: true, message: 'forbidden access' });
   }
   next();
 };
