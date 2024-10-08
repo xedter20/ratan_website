a5fb060263dcc99583e2294bbd4adcb43ac45431
xedter20 <dextermiranda441@gmail.com>
2024-10-08 14:12:15 +0800

    push

--- client_app/vite.config.js
+++ client_app/vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import commonjs from "@rollup/plugin-commonjs";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default {
-  plugins: [react(), commonjs()],
+  plugins: [react()],
   build: {
     rollupOptions: {
       external: ["formik", "yup", "fabric"],
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+      ],
     },
   },
 };
