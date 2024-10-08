45b9036d189b94312a8f33e1c2f4d2f1590e6c6b
xedter20 <dextermiranda441@gmail.com>
2024-10-08 14:01:38 +0800

    fix

--- client_app/vite.config.js
+++ client_app/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
   build: {
     rollupOptions: {
       external: ["formik"],
     },
   },
-});
+};
