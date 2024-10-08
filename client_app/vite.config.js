cb370b5855802307656bc102a979a92f3dfbf7ec
xedter20 <dextermiranda441@gmail.com>
2024-10-08 14:16:02 +0800

    add

--- client_app/vite.config.js
+++ client_app/vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["formik"], // Add formik here to externalize it
+    },
+  },
 });
