43ace0c65125d9f121f6d17cc754ae5abcfc1b18
xedter20 <dextermiranda441@gmail.com>
2024-10-08 14:10:36 +0800

    update

--- client_app/vite.config.js
+++ client_app/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import commonjs from "@rollup/plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default {
-  plugins: [react()],
+  plugins: [react(), commonjs()],
   build: {
     rollupOptions: {
       external: ["formik", "yup", "fabric"],
